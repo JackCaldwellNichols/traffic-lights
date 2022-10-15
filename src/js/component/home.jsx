9a4221948fa87a71ea1a9ae424db3b11ae390753
JackCaldwellNichols <jack.wcn@gmail.com>
2022-10-15 08:55:52 +0000

    my commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectColor, setSelectColor ] = useState ("amber")
+	const [selectPurple, setSelectPurple ] = useState ("green")
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mainDiv">
+			<div className="hanger"></div>
+			<div className="background">
+				<div onClick  = {() => setSelectColor("red")} className={"red" + (selectColor === "red" ? " glow" : "")}></div>
+				<div onClick  = {() => setSelectColor("amber")} className={"amber" + (selectColor === "amber" ? " glow" : "")}></div>
+				<div onClick  = {() => setSelectColor("green")} className={selectPurple + (selectColor === "green" ? " glow" : "")}></div>
+			</div>
+			<div className="buttonDiv">
+				<button className = "refresh" onClick={ () => selectPurple == "green" ? setSelectPurple("purple") : setSelectPurple ("green") }>Click to change from green to purple</button>
+			</div>
 		</div>
 	);
 };
